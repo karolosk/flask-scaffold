724826346cb9ebb133490d7c4af38647200c7280
karolosk <karolos.koutsoulelos@gmail.com>
2020-03-27 15:59:22 +0200

    Reading readme from setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from setuptools import setup
+from os import path
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='flaskscaffold',
-      version='0.3',
+      version='0.4',
       description='Initializing project structure for flask applications.',
       url='https://github.com/karolosk/flask-scaffold',
       author='KarolosK',
       author_email='karolos.koutsoulelos@gmail.com',
       license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       packages=['flaskscaffold'],
       zip_safe=False,
       include_package_data=True,
